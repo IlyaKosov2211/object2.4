48c709f010550d4e44965ebddcdc546cd7d86ca1
Ilya <ilakosov8@gmail.com>
2021-10-22 00:44:34 +0700

    second commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,11 +2,14 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int method(int lowerLimit, int upperLimit) {
-        int counter=0;
-        for (int i = 10; i < 99; i++) {
-            if (i * i > lowerLimit && i * i < upperLimit){
-                counter = counter + 1;
+    public int methodCheckOfNumbers(int lowerLimit, int upperLimit) {
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+            int numberOfSquare = i * i;
+            {
+                if (numberOfSquare >= lowerLimit && numberOfSquare <= upperLimit) {
+                    counter = counter + 1;
+                }
             }
         }
         return counter;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,14 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    public void shouldMethod() {
-        SQRService sqrService= new SQRService();
-        int expected = 3;
-        int lowerLimit =200;
+    public void shouldMethodCheckOfNumbers() {
+        SQRService sqrService = new SQRService();
+        int expected = 5;
+        int lowerLimit = 200;
         int upperLimit = 300;
-        int actual = sqrService.method(lowerLimit, upperLimit);
+        int actual = sqrService.methodCheckOfNumbers(lowerLimit, upperLimit);
         assertEquals(expected, actual);
-
-        }
+    }
+    @Test
+    public void shouldMethodCheckOfNumbersForExtremeValues(){
+        SQRService sqrService = new SQRService();
+        int expected = 0;
+        int lowerLimit = 10;
+        int upperLimit = 99;
+        int actual =sqrService.methodCheckOfNumbers(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldMethodCheckOfNumbersForValueErrors(){
+        SQRService sqrService=new SQRService();
+        int expected = 5;
+        int lowerLimit = 0;
+        int upperLimit = 9;
+        int actual = sqrService.methodCheckOfNumbers(lowerLimit,upperLimit);
+        assertEquals(expected,actual);
+    }
 
 }
